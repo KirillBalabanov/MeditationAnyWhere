84a91221fb87f56e0f2e09a543d2b2e27b76f29c
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-01 13:38:29 +0300

    new regex for backend validator.

--- src/main/java/com/kirillbalabanov/meditationanywhere/util/validator/UserValidator.java
+++ src/main/java/com/kirillbalabanov/meditationanywhere/util/validator/UserValidator.java
@@ -11,47 +11,35 @@ public class UserValidator {
      * Validate username. Throws {@link RegistrationException} with message of invalid part of username.
      */
     public static void isValidUsername(String username) throws RegistrationException {
-        Matcher usernameStartsWithMatcher = Patterns.USERNAME_STARTS_WITH_PATTERN.getPattern().matcher(username);
-        Matcher usernameMatcher = Patterns.USERNAME_PATTERN.getPattern().matcher(username);
-        if (username.length() < 3 || username.length() > 20)
-            throw new RegistrationException("Username should be in range of 3-20 symbols");
-        if (!usernameStartsWithMatcher.find())
-            throw new RegistrationException("Username should start with 3 normal letters");
-        if (!usernameMatcher.matches()) {
-            throw new RegistrationException(groupByRegex(Patterns.USERNAME_INVALID_SYMBOLS_PATTERN.getPattern(), username) + " - invalid symbols in username");
+        if (!Patterns.USERNAME_PATTERN.getPattern().matcher(username).matches()) {
+            throw new RegistrationException("Invalid username.");
         }
     }
 
     public static void isValidPassword(String password) throws RegistrationException {
-        Matcher passwordMatcher = Patterns.PASSWORD_PATTERN.getPattern().matcher(password);
-        if (password.length() < 3 || password.length() > 25)
-            throw new RegistrationException("Username should be in range of 3-25 symbols");
-        if (!passwordMatcher.matches()) {
-            throw new RegistrationException(groupByRegex(Patterns.PASSWORD_INVALID_SYMBOLS_PATTERN.getPattern(), password) + " - invalid symbols in username");
+        if (!Patterns.PASSWORD_PATTERN.getPattern().matcher(password).matches()) {
+            throw new RegistrationException("Invalid password.");
         }
     }
 
     public static void isValidEmail(String email) throws RegistrationException {
-        Matcher emailMatcher = Patterns.EMAIL_PATTERN.getPattern().matcher(email);
-        if (!emailMatcher.matches()) throw new RegistrationException("Invalid email");
+        if(email.length() > 320) throw new RegistrationException("Invalid email.");
+        if (!Patterns.EMAIL_PATTERN.getPattern().matcher(email).matches()) throw new RegistrationException("Invalid email.");
     }
 
     private static String groupByRegex(Pattern regex, String validationString) {
         StringBuilder invalidSymbols = new StringBuilder();
         Matcher matcher = regex.matcher(validationString);
         while (matcher.find()) {
-            invalidSymbols.append(matcher.group());
+            invalidSymbols.append("'").append(matcher.group()).append("'").append(" ");
         }
         return invalidSymbols.toString();
     }
 
     enum Patterns {
-        USERNAME_STARTS_WITH_PATTERN("^[A-Za-z]{3,}"),
-        USERNAME_PATTERN("[1-9A-Za-z _.]{3,20}"),
-        USERNAME_INVALID_SYMBOLS_PATTERN("[^1-9A-Za-z _.]"),
-        PASSWORD_PATTERN("[1-9A-Za-z _.]"),
-        PASSWORD_INVALID_SYMBOLS_PATTERN("[^1-9A-Za-z _.]"),
-        EMAIL_PATTERN("/^\\S+@\\S+\\.\\S+$/\n");
+        USERNAME_PATTERN("(?=[A-Za-z]{3,})[A-Za-z1-9 ._]{0,19}[A-Za-z1-9._]$"),
+        PASSWORD_PATTERN("(?=[A-Za-z]{3,})[A-Za-z1-9 ._]{0,19}[A-Za-z1-9._]$"),
+        EMAIL_PATTERN("^\\S+@\\S+\\.\\S+$");
 
         private final Pattern pattern;
 
--- src/test/java/com/kirillbalabanov/meditationanywhere/validator/UserValidatorTest.java
+++ src/test/java/com/kirillbalabanov/meditationanywhere/validator/UserValidatorTest.java
@@ -12,19 +12,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class UserValidatorTest {
 
     @Test
-    void isValidUsername() {
+    void isValidUsername_Valid() {
         String valid1 = "Kjf._ sfd";
-        String valid2 = "Kjf._ sfd";
+        String valid2 = "Kjf34343";
         String valid3 = "Kjf";
         String valid4 = "jjjjjjjjjjjjjjjjjjjj";
         String valid5 = "Sdfj82139 ...";
-
-
-        String invalid1 = "134 DFLKJ";
-        String invalid2 = "@#@# sdfklj";
-        String invalid3 = "Jdfjl 234 @@##@$";
-        String invalid4 = "Jd";
-        String invalid5 = "jjjjjjjjjjjjjjjjjjjjj";
+        String valid6 = "sdf..... sdlkj3434";
 
         assertDoesNotThrow(() -> {
             UserValidator.isValidUsername(valid1);
@@ -32,7 +26,19 @@ class UserValidatorTest {
             UserValidator.isValidUsername(valid3);
             UserValidator.isValidUsername(valid4);
             UserValidator.isValidUsername(valid5);
+            UserValidator.isValidUsername(valid6);
         });
+
+    }
+
+    @Test
+    void isValidUsername_Invalid() {
+        String invalid1 = "134 DFLKJ";
+        String invalid2 = "@#@# sdfklj";
+        String invalid3 = "Jdfjl 23../4 @@##@$";
+        String invalid4 = "Jd";
+        String invalid5 = "jjjjjjjjjjjjjjjjjjjjj";
+        String invalid6 = "sdf>D>><";
 
         assertThrows(RegistrationException.class, () -> {
             UserValidator.isValidUsername(invalid1);
@@ -40,26 +46,18 @@ class UserValidatorTest {
             UserValidator.isValidUsername(invalid3);
             UserValidator.isValidUsername(invalid4);
             UserValidator.isValidUsername(invalid5);
+            UserValidator.isValidUsername(invalid6);
         });
-
     }
 
     @Test
-    void isValidPassword() {
-
+    void isValidPassword_Valid() {
         String valid1 = "pass1";
         String valid2 = "Kjf._ sfd";
         String valid3 = "Kjf";
         String valid4 = "jjjjjjjjjjjjjjjjjjjj";
         String valid5 = "Sdfj82139 ...";
 
-
-        String invalid1 = "134 DFLKJ";
-        String invalid2 = "@#@# sdfklj";
-        String invalid3 = "Jdfjl 234 @@##@$";
-        String invalid4 = "Jd";
-        String invalid5 = "jjjjjjjjjjjjjjjjjjjjj";
-
         assertDoesNotThrow(() -> {
             UserValidator.isValidUsername(valid1);
             UserValidator.isValidUsername(valid2);
@@ -67,6 +65,14 @@ class UserValidatorTest {
             UserValidator.isValidUsername(valid4);
             UserValidator.isValidUsername(valid5);
         });
+    }
+    @Test
+    void isValidPassword_Invalid() {
+        String invalid1 = "134 DFLKJ";
+        String invalid2 = "@#@# sdfklj";
+        String invalid3 = "Jdfjl 234 @@##@$";
+        String invalid4 = "Jd";
+        String invalid5 = "jjjjjjjjjjjjjjjjjjjjj";
 
         assertThrows(RegistrationException.class, () -> {
             UserValidator.isValidUsername(invalid1);
@@ -74,6 +80,32 @@ class UserValidatorTest {
             UserValidator.isValidUsername(invalid3);
             UserValidator.isValidUsername(invalid4);
             UserValidator.isValidUsername(invalid5);
+        });
+    }
+
+    @Test
+    void isValidEmail_Valid() {
+        String invalid1 = "sd@cm.ua";
+        String invalid2 = "s@c.c";
+        String invalid3 = "j_j_j@j_j.c_____";
+        assertThrows(RegistrationException.class, () -> {
+            UserValidator.isValidUsername(invalid1);
+            UserValidator.isValidUsername(invalid2);
+            UserValidator.isValidUsername(invalid3);
+        });
+    }
+
+    @Test
+    void isValidEmail_Invalid() {
+        String invalid1 = " jf@c.com";
+        String invalid2 = "j @m.com";
+        String invalid3 = "j@ c.com";
+        String invalid4 = "j@c .com";
+        assertThrows(RegistrationException.class, () -> {
+            UserValidator.isValidUsername(invalid1);
+            UserValidator.isValidUsername(invalid2);
+            UserValidator.isValidUsername(invalid3);
+            UserValidator.isValidUsername(invalid4);
         });
     }
 }
