c274653dbb5c9fee0d66def13977ebe8e6c5107e
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-03 09:16:26 +0300

    Parallax on start page.

--- src/main/frontend/src/pages/StartPage.tsx
+++ src/main/frontend/src/pages/StartPage.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {MouseEventHandler, useRef, useState} from 'react';
 import {Link} from "react-router-dom";
 
 import classes from "../styles/StartPage.module.css";
 
 const StartPage = () => {
+    const speed = 0.003;
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
     return (
-        <div className={classes.bg}>
-            <div className="container">
-                <div>
-                    <h1>Meditation Any Where</h1>
-                    <Link to={"/"} className={classes.link}>start</Link>
+        <div className={classes.body} onMouseMove={(e) => {
+            const windowSizeX = window.innerWidth;
+            const windowSizeY = window.innerHeight;
+
+            const startPositionX = windowSizeX / 2;
+            const startPositionY = windowSizeY / 2;
+
+            setX((e.pageX - startPositionX) * speed);
+            setY((e.pageY - startPositionY) * speed);
+        }}>
+            <div className={classes.frame} style={{transform: "translate(" + x + "%, " + y + "%)"}}></div>
+            <div className={classes.start}>
+                <div className={classes.start__inner}>
+                    <h1 className={classes.start__title}>Meditation Any Where</h1>
+                    <Link to={"/"} className={classes.start__link}>start</Link>
                 </div>
             </div>
         </div>
