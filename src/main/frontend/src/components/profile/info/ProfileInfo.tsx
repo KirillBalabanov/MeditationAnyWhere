40877bacaea66a2e4f8935151ec96fb58f50e8d1
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-09 16:18:23 +0300

    Removed unused imports.

--- src/main/frontend/src/components/profile/info/ProfileInfo.tsx
+++ src/main/frontend/src/components/profile/info/ProfileInfo.tsx
@@ -1,18 +1,11 @@
-import React, {FC, useState} from 'react';
+import React, {FC} from 'react';
 import classes from "../Profile.module.css";
-import defaultAvatar from "../../../images/defaultAvatar.svg";
-import PopupRectangle from "../../popup/PopupRectangle";
-import timeIcon from "../../../images/timeIcon.svg";
-import Date from "../../date/Date";
-import {UserProfileI} from "../../../types/types";
-import {useNavigate} from "react-router-dom";
 
 interface ProfileInfoProps {
-    profile: UserProfileI,
     children: React.ReactNode
 }
 
-const ProfileInfo: FC<ProfileInfoProps> = ({profile, children}) => {
+const ProfileInfo: FC<ProfileInfoProps> = ({children}) => {
 
 
     return (
