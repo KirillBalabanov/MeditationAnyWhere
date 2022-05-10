e395bbc2c8ea5c20727933da29fd804bbd391b7d
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-10 13:26:26 +0300

    mouse slider done.

--- src/main/frontend/src/components/slider/Slider.tsx
+++ src/main/frontend/src/components/slider/Slider.tsx
@@ -15,6 +15,9 @@ const Slider: FC<SliderProps> = React.memo(({children, amountOfElements, element
     const [isMouseDragging, setIsMouseDragging] = useState(false);
     const [startPosition, setStartPosition] = useState(0);
 
+    const leftBoundary = useMemo(() => (elementWidth / 2) - 1, []);
+    const rightBoundary = useMemo(() => (-(elementWidth*(amountOfElements-1) + elementWidth/2)) + 1, []);
+
     const currentSlideTranslate = useMemo(() => {
         return currentSlide * -elementWidth;
     }, [currentSlide]);
@@ -23,17 +26,36 @@ const Slider: FC<SliderProps> = React.memo(({children, amountOfElements, element
         if (!isMouseDragging) {
             return;
         }
-
-        setCurrentTranslate( currentSlideTranslate + newMousePosition - startPosition);
+        let newTrans = currentSlideTranslate + newMousePosition - startPosition;
+        if(newTrans > leftBoundary) return;
+        if(newTrans < rightBoundary) return;
+        setCurrentTranslate( newTrans);
     }, [isMouseDragging, currentSlideTranslate]);
 
+    const draggingStop = useCallback(() => {
+        const moved = Math.abs(currentTranslate - currentSlideTranslate);
+        if (moved < elementWidth / 2) { // don't flip
+            setCurrentTranslate(currentSlideTranslate);
+            return;
+        }
+        if(currentTranslate < currentSlideTranslate)  { // flip to next
+            setCurrentSlide(currentSlide + 1);
+            setCurrentTranslate((currentSlide + 1) * -elementWidth);
+            return;
+        }
+        setCurrentSlide(currentSlide - 1); // flip to prev
+        setCurrentTranslate((currentSlide - 1) * -elementWidth);
+
+    }, [currentTranslate]);
+
     useEffect(() => {
-        const mouseUpHandler = () => {
+        const mouseUpHandler =(e: MouseEvent) => {
             setIsMouseDragging(false);
-        };
+            draggingStop();
+        }
         window.addEventListener("mouseup", mouseUpHandler);
         return () => window.removeEventListener("mouseup", mouseUpHandler);
-    }, []);
+    }, [currentTranslate]);
 
     return (
         <div className={classes.slider} style={{width: elementWidth  + "px"}}
