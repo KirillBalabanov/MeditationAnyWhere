3d824bc19390741a798c66eeab319c64c3f62a87
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-17 21:55:20 +0300

    new transactional operations ( get profile model, get avatar ). fixed null equals.

--- src/main/java/com/kirillbalabanov/meditationanywhere/service/ProfileService.java
+++ src/main/java/com/kirillbalabanov/meditationanywhere/service/ProfileService.java
@@ -3,6 +3,8 @@ package com.kirillbalabanov.meditationanywhere.service;
 import com.kirillbalabanov.meditationanywhere.entity.ProfileEntity;
 import com.kirillbalabanov.meditationanywhere.entity.UserEntity;
 import com.kirillbalabanov.meditationanywhere.exception.user.NoUserFoundException;
+import com.kirillbalabanov.meditationanywhere.model.AvatarModel;
+import com.kirillbalabanov.meditationanywhere.model.ProfileModel;
 import com.kirillbalabanov.meditationanywhere.repository.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,18 +19,20 @@ public class ProfileService {
     private final ProfileRepository profileRepository;
     private final UserService userService;
     private final FileService fileService;
+
     @Autowired
     public ProfileService(ProfileRepository profileRepository, UserService userService, FileService fileService) {
         this.profileRepository = profileRepository;
         this.userService = userService;
         this.fileService = fileService;
     }
+
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public ProfileEntity updateProfileSettings(long id, String bio, MultipartFile image) throws NoUserFoundException, IOException {
         UserEntity userEntity = userService.findById(id);
         ProfileEntity profileEntity = userEntity.getProfileEntity();
 
-        if (!profileEntity.getAvatarPath().equals("")) {
+        if (profileEntity.getAvatarPath() != null) {
             fileService.deleteFileFromUserDirectory(profileEntity.getAvatarPath());
         }
 
@@ -40,19 +44,35 @@ public class ProfileService {
 
         return profileRepository.save(profileEntity);
     }
+
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public ProfileEntity updateProfileSettings(long id, String bio, boolean deleteAvatar) throws NoUserFoundException {
         UserEntity userEntity = userService.findById(id);
 
         ProfileEntity profileEntity = userEntity.getProfileEntity();
 
-        if (deleteAvatar && !profileEntity.getAvatarPath().equals("")) {
-           fileService.deleteFileFromUserDirectory(profileEntity.getAvatarPath());
-            profileEntity.setAvatarUrl("");
-            profileEntity.setAvatarPath("");
+        if (deleteAvatar && profileEntity.getAvatarPath() != null) {
+            fileService.deleteFileFromUserDirectory(profileEntity.getAvatarPath());
+            profileEntity.setAvatarUrl(null);
+            profileEntity.setAvatarPath(null);
         }
         profileEntity.setBio(bio);
         return profileRepository.save(profileEntity);
     }
 
+    @Transactional(readOnly = true)
+    public ProfileModel getProfileModel(long userId) throws NoUserFoundException {
+
+        UserEntity userEntity = userService.findById(userId);
+        return ProfileModel.toModel(userEntity.getProfileEntity());
+    }
+
+    @Transactional(readOnly = true)
+    public AvatarModel getAvatar(long userId) throws NoUserFoundException {
+
+        UserEntity userEntity = userService.findById(userId);
+        ProfileEntity profileEntity = userEntity.getProfileEntity();
+
+        return new AvatarModel(profileEntity.getAvatarUrl());
+    }
 }
