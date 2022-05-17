5fa19a5655ec013cc2f6a74f511c1cc0dbe1a6e6
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-17 21:52:46 +0300

    types for frontend models

--- src/main/frontend/src/types/types.ts
+++ src/main/frontend/src/types/types.ts
@@ -1,55 +1,19 @@
-export interface ErrorI {
-    errorMsg: string
+export interface AudioInterface {
+    title: string,
+    url: string,
 }
 
-export interface UserI {
-    username: string,
-    email: string
-}
-
-export interface StatsI {
-    minListened : number,
-    sessionsListened: number,
-    currentStreak: number,
-    longestStreak: number,
-    lastSessionsDate: Date
-}
-
-export interface ProfileI {
-    bio: string,
-    avatarUrl: string
-}
-
-export interface UserProfileI {
+export interface StatsInterface {
     minListened: number,
     sessionsListened: number,
     currentStreak: number,
     longestStreak: number,
-    registrationDate: string,
-    bio: string,
-    avatarUrl: string,
-    username: string,
 }
 
-export interface AudioI {
-    audioUrl: string,
-    audioTitle: string,
+export interface AvatarInterface {
+    url: string | null,
 }
 
-export interface LoginI {
-    authenticated: boolean,
-    username: string,
-    csrf: string
-}
-
-export interface AvatarI {
-    avatarUrl: string
-}
-
-export interface CsrfI {
-    csrf: string,
-}
-
-export interface VerificationI {
-    message: string
+export interface BioInterface {
+    bio: string | null,
 }
