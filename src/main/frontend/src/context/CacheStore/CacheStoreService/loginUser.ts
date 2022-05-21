e60ecfd11603eaba00e6b2c3efd6042ecd767d57
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-21 20:43:29 +0300

    changed userDispatcher action to type: SetPrincipal

--- src/main/frontend/src/context/CacheStore/CacheStoreService/loginUser.ts
+++ src/main/frontend/src/context/CacheStore/CacheStoreService/loginUser.ts
@@ -2,13 +2,14 @@ import {AuthActionTypes} from "../../../reducer/authReducer";
 import {UserActionTypes} from "../../../reducer/userReducer";
 import {CacheStoreContextI} from "../CacheStoreContext";
 import {HeaderActionTypes} from "../../../reducer/headerReducer";
+import {UserFetchI} from "../../../types/serverTypes";
 
-export const loginUser = (username: string, cacheStore: CacheStoreContextI) => {
+export const loginUser = (userModel: UserFetchI, cacheStore: CacheStoreContextI) => {
     const [, authDispatcher] = cacheStore.authReducer;
     const [, userDispatcher] = cacheStore.userReducer;
     const [, headerDispatcher] = cacheStore.headerReducer
     userDispatcher({type: UserActionTypes.RESET_ALL} )
     authDispatcher({type: AuthActionTypes.LOGIN})
-    userDispatcher({type: UserActionTypes.SET_USERNAME, payload: username})
+    userDispatcher({type: UserActionTypes.SET_PRINCIPAL, payload: userModel})
     headerDispatcher({type: HeaderActionTypes.RELOAD_HEADER})
 }
