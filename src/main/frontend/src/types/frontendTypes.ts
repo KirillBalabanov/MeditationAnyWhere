ea4781f4058566bea029b962e5cb95b87b1718d4
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-21 20:44:24 +0300

    creation.

---
+++ src/main/frontend/src/types/frontendTypes.ts
@@ -0,0 +1,18 @@
+export interface ChangeUsernameModel {
+    username: string,
+    password: string,
+}
+
+export interface ChangeEmailModel {
+    email: string,
+    password: string,
+}
+
+export interface CodePasswordModel {
+    code: string,
+    password: string,
+}
+
+export interface PasswordModel {
+    password: string,
+}
