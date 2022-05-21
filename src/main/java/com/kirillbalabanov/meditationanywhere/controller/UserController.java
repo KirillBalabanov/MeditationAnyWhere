739a28b326a533f428db987b066d372d7bdd829a
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-21 20:31:24 +0300

    new endpoints - /change/**

--- src/main/java/com/kirillbalabanov/meditationanywhere/controller/UserController.java
+++ src/main/java/com/kirillbalabanov/meditationanywhere/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.kirillbalabanov.meditationanywhere.controller;
 
-import com.kirillbalabanov.meditationanywhere.entity.UserEntity;
+import com.kirillbalabanov.meditationanywhere.config.UserDet;
 import com.kirillbalabanov.meditationanywhere.exception.user.NoUserFoundException;
+import com.kirillbalabanov.meditationanywhere.model.EmailModel;
 import com.kirillbalabanov.meditationanywhere.model.ErrorModel;
 import com.kirillbalabanov.meditationanywhere.model.UserModel;
+import com.kirillbalabanov.meditationanywhere.model.frontend.CodePasswordModel;
+import com.kirillbalabanov.meditationanywhere.model.frontend.EmailPasswordModel;
+import com.kirillbalabanov.meditationanywhere.model.frontend.PasswordModel;
+import com.kirillbalabanov.meditationanywhere.model.frontend.UsernamePasswordModel;
 import com.kirillbalabanov.meditationanywhere.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -16,76 +19,78 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
 
 @RestController
-@RequestMapping(value = "/user/auth")
+@RequestMapping("/user")
 public class UserController {
+
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping("/registration")
-    public ResponseEntity<?> registration(@RequestBody HashMap<String, String> hm) {
-        if(hm.size() != 3) return ResponseEntity.badRequest().body("Invalid request params");
 
-        String username = hm.get("username");
-        String email = hm.get("email");
-        String password = hm.get("password");
-
-        UserEntity registeredUser;
+    @PutMapping("/change/username")
+    public ResponseEntity<?> changeUsername(@RequestBody UsernamePasswordModel usernameModel) {
+        UserDet userDet = (UserDet) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserModel userModel;
         try {
-            registeredUser = userService.register(username, email, password);
+            userModel = userService.changeUsername(userDet.getUserId(), usernameModel.username(), usernameModel.password());
         } catch (Exception e) {
-            return ResponseEntity.ok().body(ErrorModel.fromMessage(e.getMessage()));
+            return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(ErrorModel.fromMessage(e.getMessage()));
         }
-        return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(UserModel.toModel(registeredUser));
+        return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(userModel);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody HashMap<String, String> hashMap) {
-        if(hashMap.size() != 2) return ResponseEntity.badRequest().body("Invalid request params");
-        String username = hashMap.get("username");
-        String password = hashMap.get("password");
-
-        HashMap<Object, Object> hm = new HashMap<>();
+    @PutMapping("/change/email")
+    public ResponseEntity<?> changeEmailRequest(@RequestBody EmailPasswordModel emailPasswordModel) {
+        UserDet userDet = (UserDet) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserModel userModel;
         try {
-            userService.isAbleToLogIn(username, password);
+            userModel = userService.changeEmailRequest(userDet.getUserId(), emailPasswordModel.email(), emailPasswordModel.password());
         } catch (Exception e) {
-            return ResponseEntity.ok().body(ErrorModel.fromMessage(e.getMessage()));
+            return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(ErrorModel.fromMessage(e.getMessage()));
         }
-
-        hm.put("username", username);
-        // generate new csrf token
-
-        // set auth.
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username,
-                password);
-        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-
-        return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(hm);
+        return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(userModel);
     }
 
-    @PostMapping("/logout")
-    public ResponseEntity<?> logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+    @PutMapping("/change/email/verify")
+    public ResponseEntity<?> changeEmailVerification(@RequestBody CodePasswordModel codePasswordModel) {
+        UserModel userModel;
+        try {
+            userModel = userService.changeEmailVerification(codePasswordModel.code(), codePasswordModel.password());
+        } catch (Exception e) {
+            return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(ErrorModel.fromMessage(e.getMessage()));
+        }
+        return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(userModel);
+    }
+
+    @GetMapping("/change/email/code-exists/{code}")
+    public ResponseEntity<?> codeExists(@PathVariable String code) {
+        EmailModel emailModel;
+        try {
+            emailModel = userService.getEmailByCode(code);
+        } catch (NoUserFoundException e) {
+            return ResponseEntity.ok().cacheControl(CacheControl.noCache()).body(ErrorModel.fromMessage(e.getMessage()));
+        }
+        return ResponseEntity.ok().cacheControl(CacheControl.noCache()).body(emailModel);
+    }
+
+    @DeleteMapping("/delete/account")
+    public ResponseEntity<?> deleteUser(@RequestBody PasswordModel passwordModel, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        UserDet userDet = (UserDet) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserModel deletedModel;
+        try {
+            deletedModel = userService.deleteAccountByUserId(userDet.getUserId(), passwordModel.password());
+        } catch (Exception e) {
+            return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(ErrorModel.fromMessage(e.getMessage()));
+        }
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         new SecurityContextLogoutHandler().logout(httpServletRequest, httpServletResponse, authentication);
 
-        return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(null);
-    }
-    @GetMapping("/verification/{activationCode}")
-    public ResponseEntity<?> verification(@PathVariable String activationCode) {
-        HashMap<String, String> hashMap = new HashMap<>();
-        try {
-            userService.verifyUserByActivationCode(activationCode);
-        } catch (NoUserFoundException e) {
-            return ResponseEntity.ok().body(ErrorModel.fromMessage(e.getMessage()));
-        }
-        hashMap.put("message", "Account is successfully activated!");
-        return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(hashMap);
+        return ResponseEntity.ok().cacheControl(CacheControl.noStore()).body(deletedModel);
     }
 
 }
