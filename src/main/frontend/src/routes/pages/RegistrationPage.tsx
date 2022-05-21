c3a5e6ca6240ff10982bf654c58b28b4ad90dddf
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-21 20:38:52 +0300

    fixed logic, included new FormInputContainer component.

--- src/main/frontend/src/routes/pages/RegistrationPage.tsx
+++ src/main/frontend/src/routes/pages/RegistrationPage.tsx
@@ -2,7 +2,7 @@ import React, {FC, FormEvent, useCallback, useState} from 'react';
 import classes from "../styles/AuthPage.module.css";
 import {Link} from "react-router-dom";
 import {useAuthRedirect} from "../../hooks/useAuthRedirect";
-import Form, {FormStyles} from "../../components/form/Form";
+import Form, {FormState} from "../../components/form/Form";
 import FormTitle from "../../components/form/FormTitle";
 import FormInput from "../../components/form/FormInput";
 import {validFormInput, ValidFormValidator} from "../../components/form/FormService/validFormInput";
@@ -13,6 +13,7 @@ import {isValidPassword} from "../../util/UserValidator/isValidPassword";
 import {useCacheStore} from "../../context/CacheStore/CacheStoreContext";
 import {ErrorFetchI, UserFetchI} from "../../types/serverTypes";
 import {csrfFetching, FetchContentTypes, FetchingMethods} from "../../util/Fetch/csrfFetching";
+import FormInputContainer from "../../components/form/FormInputContainer";
 
 
 const RegistrationPage: FC = () => {
@@ -21,32 +22,34 @@ const RegistrationPage: FC = () => {
 
     useAuthRedirect(authState.auth);
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [formClasses, setFormClasses] = useState<FormStyles[]>([]);
+    const [formState, setFormState] = useState<FormState>(FormState.DEFAULT);
 
-    const [errorMsg, setErrorMsg] = useState("");
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
 
     const postRegister = useCallback((e: FormEvent) => {
         e.preventDefault();
-        let children = (e.target as Element).children;
 
-        let username = (children[1] as HTMLInputElement).value;
-        let email = (children[2] as HTMLInputElement).value;
-        let password = (children[3] as HTMLInputElement).value;
+        let usernameInput = (e.currentTarget.querySelector("input[name=username]") as HTMLInputElement);
+        let emailInput = (e.currentTarget.querySelector("input[name=email]") as HTMLInputElement);
+        let passwordInput = (e.currentTarget.querySelector("input[name=password]") as HTMLInputElement);
+
+        let username = usernameInput.value;
+        let email = emailInput.value;
+        let password = passwordInput.value;
         try {
             isValidUsername(username);
             isValidEmail(email)
             isValidPassword(password);
-            setErrorMsg("");
+            setErrorMsg(null);
         } catch (e) {
             if(e instanceof Error){
                 setErrorMsg(e.message);
             }
             return;
         }
-        setFormClasses([FormStyles.loading]);
-        setIsLoading(true);
+        setFormState(FormState.LOADING);
+
         let body = JSON.stringify({
             "username": username,
             "email": email,
@@ -65,31 +68,34 @@ const RegistrationPage: FC = () => {
             else {
                 setErrorMsg("Verification email has been sent.");
                 // clear inputs
-                (children[1] as HTMLInputElement).value = "";
-                (children[2] as HTMLInputElement).value = "";
-                (children[3] as HTMLInputElement).value = "";
+                usernameInput.value = "";
+                emailInput.value = "";
+                passwordInput.value  = "";
             }
-            animateFetchRequest(setIsLoading, setFormClasses, failed);
+            animateFetchRequest(setFormState, failed);
         });
     }, []);
 
 
     return (
-        <div>
+        <div className={classes.auth}>
             <div className={classes.auth__outer}>
-                <Form formStyle={formClasses} submitCallback={postRegister} isLoadingRequest={isLoading}
+                <Form formState={formState} submitCallback={postRegister}
                       errorMsg={errorMsg} buttonTitle={"Register"}>
 
                     <FormTitle title={"Create account"}></FormTitle>
-                    <FormInput placeholder={"Input username"} type={"text"} name={"username"}
-                               onInput={(e) => validFormInput(e, ValidFormValidator.username, setErrorMsg)}
-                    />
-                    <FormInput placeholder={"Input email"} type={"email"} name={"email"}
-                               onInput={(e) => validFormInput(e, ValidFormValidator.email, setErrorMsg)}
-                    />
-                    <FormInput placeholder={"Input password"} type={"password"} name={"password"}
-                               onInput={(e) => validFormInput(e, ValidFormValidator.password, setErrorMsg)}
-                    />
+                    <FormInputContainer>
+                        <FormInput placeholder={"Input username"} type={"text"} name={"username"}
+                                   onInput={(e) => validFormInput(e, ValidFormValidator.username, setErrorMsg)}
+                        />
+                        <FormInput placeholder={"Input email"} type={"email"} name={"email"}
+                                   onInput={(e) => validFormInput(e, ValidFormValidator.email, setErrorMsg)}
+                        />
+                        <FormInput placeholder={"Input password"} type={"password"} name={"password"}
+                                   onInput={(e) => validFormInput(e, ValidFormValidator.password, setErrorMsg)}
+                        />
+                    </FormInputContainer>
+
                 </Form>
                 <Link to={"/login"} className={classes.auth__link}>log in</Link>
             </div>
