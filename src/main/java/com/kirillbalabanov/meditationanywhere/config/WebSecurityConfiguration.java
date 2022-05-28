af5dde733160d103960285d4d866b5ecdb4e46a1
KirillBalabanov <kirillbalabanov77711777@gmail.com>
2022-05-28 16:46:03 +0300

    new AuthenticationManager bean

--- src/main/java/com/kirillbalabanov/meditationanywhere/config/WebSecurityConfiguration.java
+++ src/main/java/com/kirillbalabanov/meditationanywhere/config/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.kirillbalabanov.meditationanywhere.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,5 +39,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(this.userDetService);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
